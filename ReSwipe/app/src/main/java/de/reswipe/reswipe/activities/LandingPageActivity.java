aa0cc2fbd864da9a6d1a48bd6f4304b036dddb1f
Daniel Weber <wedaniel1992@gmail.com>
2017-05-29 19:47:08 +0200

    firebase write example

--- ReSwipe/app/src/main/java/de/reswipe/reswipe/activities/LandingPageActivity.java
+++ ReSwipe/app/src/main/java/de/reswipe/reswipe/activities/LandingPageActivity.java
@@ -2,6 +2,9 @@ package de.reswipe.reswipe.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.*;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import de.reswipe.reswipe.R;
 
@@ -11,5 +14,11 @@ public class LandingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("recipes");
+
+        myRef.setValue("Hello, World!");
     }
+
 }
