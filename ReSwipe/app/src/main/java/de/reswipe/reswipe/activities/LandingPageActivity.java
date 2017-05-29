3896b94304d3822b14750cabb8470b867d14cfee
Daniel Weber <wedaniel1992@gmail.com>
2017-05-29 19:56:56 +0200

    get data from firebase

--- ReSwipe/app/src/main/java/de/reswipe/reswipe/activities/LandingPageActivity.java
+++ ReSwipe/app/src/main/java/de/reswipe/reswipe/activities/LandingPageActivity.java
@@ -2,9 +2,13 @@ package de.reswipe.reswipe.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.*;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.reswipe.reswipe.R;
 
@@ -19,6 +23,23 @@ public class LandingPageActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("recipes");
 
         myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("onDataChange", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("onCancelled", "Failed to read value.", error.toException());
+            }
+        });
     }
 
 }
